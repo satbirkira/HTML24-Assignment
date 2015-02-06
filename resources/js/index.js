ba2f3105fd64daee25d4dd75ab44a91caf894cbc
Satbir Saini <satbir.kira@gmail.com>
2015-02-05 19:16:54 -0500

    Added search bar

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,18 @@
 //add slider on doc load
 $(function() 
 {
+	//add slider
     $(".rslides").responsiveSlides({
 		auto: true,             // Boolean: Animate automatically, true or false
 		speed: 400,            // Integer: Speed of the transition, in milliseconds
 		timeout: 3000,          // Integer: Time between slide transitions, in milliseconds
 	});
+	
+	//add search box
+	var availableTags = ["Apple", "Beer", "Club Soda", "Dairy"
+    ];
+    $( "#search_bar" ).autocomplete({
+      source: availableTags
+    });
+	$("#search_bar").corner();
 });
